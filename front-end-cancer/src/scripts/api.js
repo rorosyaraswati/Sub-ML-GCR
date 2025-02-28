31889ee03a1ba96e33de8f0e5e67f4d47ec10186
rorosyaraswati <rorosyaraswati@gmail.com>
2025-02-28 21:46:13 +0700

    Add front-end-cancer as a normal folder

---
+++ front-end-cancer/src/scripts/api.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://machine-learning-cancer-nwls4hgt2q-et.a.run.app';
+
+const ENDPOINT = {
+  predict: `${BASE_URL}/predict`,
+};
+
+class PredictAPI {
+  static async predict(data) {
+    const response = await fetch(ENDPOINT.predict, {
+      method: 'POST',
+      body: data,
+      redirect: 'follow',
+    });
+
+    const json = await response.json();
+    return json;
+  }
+}
